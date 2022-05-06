b3afec2800e719e6ab74ece5d92360eb0e47c110
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2022-05-06 17:16:32 -0300

    :sparkles: Projeto backend inicial

---
+++ backengsoft/src/main/java/com/java/backengsoft/BackengsoftApplication.java
@@ -0,0 +1,13 @@
+package com.java.backengsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackengsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackengsoftApplication.class, args);
+	}
+
+}
